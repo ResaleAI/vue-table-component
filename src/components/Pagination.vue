01970a83b38b3c4b5db8678bb7044731e092e575
BJ Bryant <bj@resaleai.com>
2017-11-15 15:22:40 -0600

    change pagination to prev and next buttons

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,8 +1,15 @@
 <template>
     <nav v-if="shouldShowPagination">
         <ul class="pagination justify-content-center">
-            <li class="page-item" :class="{ active: isActive(page) }" v-for="page in pages">
-                <a class="page-link" @click="pageClicked(page)">{{ page }}</a>
+            <li class="page-item" :class="{'disabled': previousDisabled}">
+                <a class="page-link" @click="previousClicked">
+                    <span slot="pagination-previous-content">&lt;</span>
+                </a>
+            </li>
+            <li class="page-item" :class="{'disabled': nextDisabled}">
+                <a class="page-link" @click="nextClicked">
+                    <span slot="pagination-next-content">&gt;</span>
+                </a>
             </li>
         </ul>
     </nav>
@@ -37,6 +44,26 @@
 
                 return this.pagination.totalPages > 1;
             },
+
+            currentPage () {
+                return this.pagination.currentPage || 1;
+            },
+
+            totalPages () {
+                let ret = 0
+                if (!!this.pagination && !!this.pagination.totalPages) {
+                    ret = this.pagination.totalPages
+                }
+                return ret
+            },
+
+            previousDisabled () {
+                return this.currentPage === 1;
+            },
+
+            nextDisabled () {
+                return this.currentPage === this.totalPages
+            }
         },
 
         methods: {
@@ -53,6 +80,22 @@
 
                 this.$emit('pageChange', page);
             },
+
+            previousClicked () {
+                const page = this.currentPage - 1
+                const previousAllowed = page > 0
+                if (previousAllowed) {
+                    this.$emit('pageChange', page)
+                }
+            },
+
+            nextClicked () {
+                const page = this.currentPage + 1
+                const nextAllowed = page <= this.totalPages
+                if (nextAllowed) {
+                    this.$emit('pageChange', page)
+                }
+            }
         },
     };
 </script>
